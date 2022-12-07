9272e146b416d37af4cb3e0d90317a9197b99b95
pogyomo <abc_abc_bcd_bcd@yahoo.co.jp>
2022-12-07 16:00:40 +0900

    Add module to execute command.

---
+++ lua/play/player/mpv/command.lua
@@ -0,0 +1,89 @@
+local socket = require("play.pipe.socket")
+local pair   = require("play.pipe.pair")
+
+---Create a command.
+---@param name string Name of command.
+---@param params any | any[] Parameters of this command.
+---@param id integer Request id.
+---@return string
+local function create_command(name, params, id)
+    params = type(params) == "table" and params or { params } -- When params not table, change it to table.
+    local command = { command = { name }, request_id = id }
+    for _, param in ipairs(params) do
+        table.insert(command.command, param)
+    end
+    return vim.fn.json_encode(command) .. "\n"
+end
+
+---Execute mpv command asynchronously.
+---@class MpvCommandExecutor
+---@field socket SocketHandler Reciever message from mpv.
+---@field pair PairPipeHandler Handle message asynchronously.
+---@field path string Path to socket.
+local M = {}
+
+---Create a new MpvCommandExecutor instance.
+---@param path string Path to socket.
+---@return MpvCommandExecutor
+function M:new(path)
+    return setmetatable({
+        socket = socket:new(),
+        pair   = pair:new(),
+        path   = path,
+    }, {
+        __index = self
+    })
+end
+
+---Execute command.
+---@param name string Name of command.
+---@param params any | any[] Parameters of this command.
+---@param on_success? fun(data: table) Called when command is executed successfully.
+function M:execute(name, params, on_success)
+    self.pair:connect()
+    self.socket:connect(self.path)
+    self.socket:read_start(function(data)
+        self.pair:write(data)
+    end)
+
+    local id = math.floor(math.random(1e10)) -- Get a unique id.
+    self.socket:write(create_command(name, params, id))
+    self.pair:read_start(function(data)
+        for d in data:gmatch("[^\n]+") do -- When a reply have multiple message, split it by newline.
+            d = vim.fn.json_decode(d)
+            if d.request_id == id then
+                self.pair:disconnect()
+                self.socket:disconnect()
+                if d.error == "success" then
+                    if on_success then
+                        on_success(d)
+                    end
+                else
+                    error(("Failed to execute command '%s': %s"):format(name, d.error))
+                end
+            end
+        end
+    end)
+end
+
+---Change property by using given value
+---@generic T
+---@param name string Name of the property.
+---@param value T Value to set.
+---@param on_success? fun(data: table) Called when command is executed successfully.
+function M:change_property(name, value, on_success)
+    self:execute("set_property", { name, value }, on_success)
+end
+
+---Change property changer.
+---@generic T
+---@param name string Name of the property.
+---@param changer fun(property: T): T
+---@param on_success? fun(data: table) Called when command is executed successfully.
+function M:change_property_with(name, changer, on_success)
+    self:execute("get_property", name, function(data)
+        self:change_property(name, changer(data.data), on_success)
+    end)
+end
+
+return M
--- lua/play/player/mpv/init.lua
+++ lua/play/player/mpv/init.lua
@@ -1,27 +1,10 @@
-local job    = require("play.job")
-local socket = require("play.pipe.socket")
-local pair   = require("play.pipe.pair")
+local job = require("play.job")
+local cmd = require("play.player.mpv.command")
 local socket_path = "/tmp/mpvsocket"
-
----Create a command.
----@param name string Name of command.
----@param params any | any[] Parameters of this command.
----@param id integer Request id.
----@return string
-local function create_command(name, params, id)
-    params = type(params) == "table" and params or { params } -- When params not table, change it to table.
-    local command = { command = { name }, request_id = id }
-    for _, param in ipairs(params) do
-        table.insert(command.command, param)
-    end
-    return vim.fn.json_encode(command) .. "\n"
-end
 
 ---A simple handler of mpv.
 ---@class MpvHandler
 ---@field mpv_job JobHandler JobHandler to handle mpv.
----@field socket SocketHandler Socket to communicate with mpv.
----@field pair PairPipeHandler PairPipe to treat reply which mpv send.
 local M = {}
 
 ---Create a new MpvHandler instance.
@@ -29,8 +12,6 @@ local M = {}
 function M:new()
     return setmetatable({
         mpv_job = job:new(),
-        socket  = socket:new(),
-        pair    = pair:new(),
     }, {
         __index = self
     })
@@ -55,47 +36,38 @@ function M:start_file_without_video(path)
             self:stop()
         end
     })
-    self.pair:connect()
-    vim.defer_fn(function()
-        self.socket:connect(socket_path)
-        self.socket:read_start(function(data)
-            self.pair:write(data)
-        end)
-    end, 1000)
 end
 
 ---Stop playback and exit from mpv.
 function M:stop()
-    self.pair:disconnect()
-    self.socket:disconnect()
     self.mpv_job:stop()
 end
 
 ---Load file from given path/url.
 ---@param path string Path or url to load.
 function M:loadfile(path)
-    self:__exec_command("loadfile", vim.fn.expand(path))
+    cmd:new(socket_path):execute("loadfile", vim.fn.expand(path))
 end
 
 ---Seek playback time by given diff.
 ---@param diff integer
 function M:seek(diff)
-    self:__exec_command("seek", diff)
+    cmd:new(socket_path):execute("seek", diff)
 end
 
 ---Pause the playback.
 function M:pause()
-    self:__change_property("pause", true)
+    cmd:new(socket_path):change_property("pause", true)
 end
 
 ---Resume the playback.
 function M:resume()
-    self:__change_property("pause", false)
+    cmd:new(socket_path):change_property("pause", false)
 end
 
 ---Toggle pause and resume.
 function M:toggle()
-    self:__change_property_by_using_current("pause", function(property)
+    cmd:new(socket_path):change_property_with("pause", function(property)
         return not property
     end)
 end
@@ -103,55 +75,8 @@ end
 ---Increase/Decrease the volume by given diff.
 ---@param diff integer How much to change volume.
 function M:volume(diff)
-    self:__change_property_by_using_current("volume", function(property)
+    cmd:new(socket_path):change_property_with("volume", function(property)
         return property + diff
-    end)
-end
-
----Execute command.
----@param name string Name of command.
----@param params any | any[] Parameters of this command.
----@param on_success? fun(data: table) Called when command is executed successfully.
-function M:__exec_command(name, params, on_success)
-    if not self:is_running() then
-        return
-    end
-
-    local id = math.floor(math.random(1e10)) -- Get a unique id.
-    self.socket:write(create_command(name, params, id))
-    self.pair:read_start(function(data)
-        for d in data:gmatch("[^\n]+") do -- When a reply have multiple message, split it by newline.
-            d = vim.fn.json_decode(d)
-            if d.error ~= "success" and d.request_id == id then
-                self.pair:read_stop()
-                error(("Failed to execute command '%s': %s"):format(name, d.error))
-            elseif d.error == "success" and d.request_id == id then
-                self.pair:read_stop()
-                if on_success then
-                    on_success(d)
-                end
-            end
-        end
-    end)
-end
-
----Change property by using given value
----@generic T
----@param name string Name of the property.
----@param value T Value to set.
----@param on_success? fun(data: table) Called when command is executed successfully.
-function M:__change_property(name, value, on_success)
-    self:__exec_command("set_property", { name, value }, on_success)
-end
-
----Change property by using current property.
----@generic T
----@param name string Name of the property.
----@param changer fun(property: T): T
----@param on_success? fun(data: table) Called when command is executed successfully.
-function M:__change_property_by_using_current(name, changer, on_success)
-    self:__exec_command("get_property", name, function(data)
-        self:__change_property(name, changer(data.data), on_success)
     end)
 end
 
